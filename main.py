dd5047bbe12293a330d4f5e3d676b61b39e34bee
HulkHogan6262 <hulkhogan6262@gmail.com>
2024-02-25 19:40:40 +0100

    Adding get_scores routes to the main file

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sanic
 import dotenv
+
 from routes.register_score import register_score
+from routes.get_score import get_score
 
 dotenv.load_dotenv(".env")
 
 app = sanic.Sanic("SpaceInvadersAPI-v2")
 
 app.add_route(register_score, "/register-score", methods=["POST"])
+app.add_route(get_score, "/get-score", methods=["GET"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, workers=4, access_log=True)
